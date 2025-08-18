5375b458b7e30f2eab00fe0387e00ce279b7fe96
Zaid <zaedalsalh123qqq111@gmail.com>
2025-08-18 15:40:02 +0300

    Models Createed

--- project/MyApp/admin.py
+++ project/MyApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from MyApp.models import Userr , Services , UserRating , ServiceRequest , Notifications
 
-# Register your models here.
+admin.site.register(Userr)
+admin.site.register(Services)
+admin.site.register(UserRating)
+admin.site.register(ServiceRequest)
+admin.site.register(Notifications)
--- project/MyApp/models.py
+++ project/MyApp/models.py
@@ -1,3 +1,52 @@
 from django.db import models
 
-# Create your models here.
+
+class Services(models.Model):
+    ServiceName = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.ServiceName
+
+
+class Userr(models.Model):
+    FullName = models.CharField(max_length=255)
+    Email = models.EmailField(unique=True)
+    Password = models.CharField(max_length=128)
+    TypeOfService = models.ForeignKey(Services, on_delete=models.CASCADE)
+    PhoneNumber = models.IntegerField(null=True, blank=True)
+    YearsOfExperience = models.IntegerField(null=True, blank=True)
+    Location = models.CharField(null=True, blank=True)
+    img = models.ImageField(upload_to='UserImg', null=True, blank=True)
+    IsNotifications = models.BooleanField(default=True)
+    IsServices = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'{self.FullName} - {self.TypeOfService}'
+
+
+class UserRating(models.Model):
+    UserId = models.ForeignKey(Userr, on_delete=models.CASCADE)
+    Evaluation = models.FloatField()
+
+    def __str__(self):
+        return f'{self.UserId} - {self.Evaluation}'
+
+
+class Notifications(models.Model):
+    UserId = models.ForeignKey(Userr, on_delete=models.CASCADE)
+    Title = models.CharField(max_length=255)
+    Description = models.CharField(max_length=500)
+    Isrequest = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.Title} - {self.UserId}'
+
+
+class ServiceRequest(models.Model):
+    IdUser = models.ForeignKey(Userr, on_delete=models.CASCADE, related_name='requests_made')
+    IdClient = models.ForeignKey(Userr, on_delete=models.CASCADE, related_name='requests_received')
+    Location = models.CharField(null=True, blank=True)
+    HourlyPrice = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.IdUser} -> {self.IdClient} ({self.HourlyPrice})'
