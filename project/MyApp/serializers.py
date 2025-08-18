3070682536781eaf04554b63fbbf7d657d7e0a77
Zaid <zaedalsalh123qqq111@gmail.com>
2025-08-18 15:56:23 +0300

    Create Serializers

---
+++ project/MyApp/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from .models import Userr, UserRating, Notifications, ServiceRequest
+
+
+class NotificationsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Notifications
+        fields = '__all__'
+
+
+class ServiceRequestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ServiceRequest
+        fields = '__all__'
+
+
+class UserrSerializer(serializers.ModelSerializer):
+    notifications = NotificationsSerializer(source='notifications_set', many=True, read_only=True)
+    requests_made = ServiceRequestSerializer(source='requests_made', many=True, read_only=True)
+    requests_received = ServiceRequestSerializer(source='requests_received', many=True, read_only=True)
+
+    class Meta:
+        model = Userr
+        fields = '__all__'
