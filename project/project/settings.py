1910b06a7d048b0f95ebc67ef8c7705ce6902916
Zaid <zaedalsalh123qqq111@gmail.com>
2025-08-23 16:11:32 +0300

    requirements.txt Procfile

--- project/project/settings.py
+++ project/project/settings.py
@@ -13,9 +13,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h0m(per!_%*v^miixtvd%-jib65i+p1az&$zv6&n5r@g%11imi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -151,5 +151,4 @@ SIMPLE_JWT = {
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
 }
-
 
