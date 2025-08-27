1865a6cd1eb28a71b52d1aa826852f10ec6a334f
Zaid <zaedalsalh123qqq111@gmail.com>
2025-08-28 02:33:54 +0300

    1q1

--- project/project/settings.py
+++ project/project/settings.py
@@ -141,9 +141,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'zaedalsalh123qqq111@gmail.com'
-EMAIL_HOST_PASSWORD = 'ezzq efjp auhb kevs'
-
+EMAIL_HOST_USER = os.getenv('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASS')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
 # وقت التوكن 
